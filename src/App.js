04a57dcab6992491a1e6c98301761177208bfa31
Sergkras <seakras@ukr.net>
2021-06-04 15:54:31 +0300

    rules

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import "./App.css";
 
-import Profile from './components/profile/profile'
-import Statistics from './components/statistics/statistics-data';
-import FriendList from './components/friend-list/friend-list';
-import TransactionHistory from './components/transaction-history/transaction-history-data';
+import Profile from './components/Profile'
+import Statistics from './components/Statistics';
+import FriendList from './components/Friendlist';
+import TransactionHistory from './components/TransactionHistory';
 
-import user from './components/profile/user.json';
-import statisticalData from './components/statistics/statistical-data.json';
-import friends from './components/friend-list/friends.json';
-import transactions from './components/transaction-history/transactions.json';
+import user from './components/Profile/dataUser/user.json';
+// import statisticalData from './components/Statistics.json';
+// import friends from './components/friend-list/friends.json';
+// import transactions from './components/transaction-history/transactions.json';
 
 const App = () => {
   return (
