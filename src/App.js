0133150de2aae63f0672e7eff31ceccd082f3183
Sergkras <seakras@ukr.net>
2021-06-04 13:21:11 +0300

    next2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import "./App.css";
+
+import Profile from './components/profile/profile'
+import Statistics from './components/statistics/statistics-data';
+import FriendList from './components/friend-list/friend-list';
+import TransactionHistory from './components/transaction-history/transaction-history-data';
+
+import user from './components/profile/user.json';
+import statisticalData from './components/statistics/statistical-data.json';
+import friends from './components/friend-list/friends.json';
+import transactions from './components/transaction-history/transactions.json';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Profile
+        avatar={user.avatar}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        // followers={user.stats.followers}
+        // views={user.stats.views}
+        // likes={user.stats.likes}
+        stats={user.stats}
+
+      />
+
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
+    </>
   );
-}
+};
 
 export default App;
